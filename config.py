d6f5909b371553213546650847312773c566558d
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-05 23:42:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Haeszm/Adin",
+    "https://github.com/jcjxjdjdds/jekaa145",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MF_CD")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MF_CW")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/S_MA4")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/S_MA4")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/1efdc3c274832806953f2.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/e15b16465f42229456e72.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
